8f9556e37f87b4034936d46c499dd543924eb323
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2024-04-03 19:01:18 +0700

    Showing data in activity main

--- app/src/main/java/com/yunpnzr/quoteapp/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/yunpnzr/quoteapp/presentation/main/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.yunpnzr.quoteapp.presentation.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.yunpnzr.quoteapp.data.repository.QuoteRepository
+import kotlinx.coroutines.Dispatchers
 
 class MainViewModel(private val repository: QuoteRepository): ViewModel() {
     //todo : tugas ambil data quotes
-
+    fun getData() = repository.getRandomQuotes().asLiveData(Dispatchers.IO)
 }
